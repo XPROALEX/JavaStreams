690c0a63f6fb059b53b15c8ab7d325d5e3b88498
Alex <alex97av@gmail.com>
2023-09-08 14:03:52 +0200

    fine

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class Tester {
     public static void main(String[] args) {
+        try {
+            String fileTxt = "src/file.txt";
+            Stream<String> fileTxtReading = Files.lines(Path.of(fileTxt));
+            fileTxtReading.forEach(text -> System.out.println(text));
+        } catch (IOException ioException) {
+            System.err.println("Error!");
+            ioException.printStackTrace();
+        }
     }
 }
